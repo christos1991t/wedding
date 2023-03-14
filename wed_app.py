0b526c3c943747a8b7b8414e4a5bc6495f899961
christopaldis <cjgh2007@gmail.com>
2023-03-14 15:54:53 +0100

    full working guest management

--- wed_app.py
+++ wed_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import pandas as pd
+import io
+
+
+buffer = io.BytesIO()
 
 st.set_page_config(page_title="Γαμος Νικου - Βασιλικης", layout="wide")
 st.title("Λιστα καλεσμενων")
@@ -7,9 +11,8 @@ st.title("Λιστα καλεσμενων")
 # Read data from Excel file
 df = pd.read_excel("wedding.xlsx")
 df = df.fillna('')
-df['Τηλεφωνο'] = df['Τηλεφωνο'].astype(str).str.replace('[,.]', '')
+df['Τηλεφωνο'] = df['Τηλεφωνο'].astype(str).str.rstrip('.0')
 
-# Get unique values for the 'Επωνυμο' column
 column_values = df['Επωνυμο '].tolist()
 
 # Allow user to select one or multiple values from the 'Επωνυμο' column
@@ -19,13 +22,28 @@ selected_values = st.multiselect('Επιλογη επωνυμου καλεσμε
 if selected_values:
     filtered_df = df[df['Επωνυμο '].isin(selected_values)]
     edited_df = st.experimental_data_editor(filtered_df, num_rows="dynamic")
-    if st.button('Save changes'):
+    if st.button('Αποθηκευση Αλλαγων'):
         # Update the original Excel file with the changes made in the data editor
         df.update(edited_df)
         df.to_excel("wedding.xlsx", index=False)
+        st.success("Η αλλαγες αποθηκευτικαν")
 else:
     edited_df = st.experimental_data_editor(df, num_rows="dynamic")
-    if st.button('Save changes'):
+    if st.button('Αποθηκευση Αλλαγων'):
         # Update the original Excel file with the changes made in the data editor
         df.update(edited_df)
         df.to_excel("wedding.xlsx", index=False)
+        st.success("Η αλλαγες αποθηκευτικαν")
+
+with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+    # Write each dataframe to a different worksheet.
+    edited_df.to_excel(writer, sheet_name='Sheet1')
+      # Close the Pandas Excel writer and output the Excel file to the buffer
+    writer.save()
+
+    st.download_button(
+        label="Κατεβασμα Λιστας",
+        data=buffer,
+        file_name="Λιστα Καλεσμενων.xlsx",
+        mime="application/vnd.ms-excel"
+    )
